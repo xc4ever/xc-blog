201282aaf5b8b1bdfc48ca6ee54d96fbf76ded52
xc4ever <184882726+xc4ever@users.noreply.github.com>
2026-08-19 11:57:58 +0000

    Deploying to pages from @ xc4ever/xc-blog@727d6dca04eec9ddec17d581bb26059823b399c3 🚀

---
+++ _astro/url-utils.BlqXKabk.js
@@ -0,0 +1,1 @@
+import"./translation.CXwjewqG.js";import"./config.DI8V3gWJ.js";import"./date-utils.Bk93_ato.js";function i(r,e){return r.replace(/^\/|\/$/g,"").toLowerCase()===e.replace(/^\/|\/$/g,"").toLowerCase()}function t(...r){return r.join("/").replace(/\/+/g,"/")}function p(r){return t("","/",r)}export{p as n,i as t};
